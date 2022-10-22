891f2b1cd94364fe52c9590c306d8b54a5579be2
Junior Quispe Alegria <78713670+dayeljr@users.noreply.github.com>
2022-10-22 10:48:28 -0400

    building web api on authors

---
+++ WebAPIAutores/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
---
+++ WebAPIAutores/Controllers/AutoresController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() {
+                new Autor() { Id = 1, Nombre = "Dayel" },
+                new Autor() { Id = 2, Nombre = "Junior" }
+            };
+        }
+    }
+}
